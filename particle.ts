bb16782d4bce005967fc9a7876864ae7488eee3f
nasafrasa <86539501+Nasafrasa@users.noreply.github.com>
2023-04-10 21:37:14 -0700

    Added Stuff

--- particle.ts
+++ particle.ts
@@ -1,63 +1,87 @@
-import { KeyframesLinear, Wall, KeyframesVec3, KeyframesVec4, Interpolation, rand, Vec3, arrAdd, rotatePoint } from "https://deno.land/x/remapper@3.1.1/src/mod.ts";
+import { Wall } from "./wall.ts";
+import { CustomEvent } from "./custom_event.ts";
+import { rand, Vec3, arrAdd, rotatePoint } from "./general.ts";
+import { KeyframesLinear, KeyframesVec3, KeyframesVec4, Interpolation } from "./animation.ts";
 
 // Generator Features
 let generatorTime: number;
 let generatorDuration: number;
 let generatorParticleAmount: number;
 
-let particleStartThreshold = 0.1;
-let particleEndThreshold = 0.1;
 let particleLife = 1;
 let particleDistance = 1;
 let particleEasing = "easeLinear" as Interpolation;
 
-let generatorType = "Point" as ParticleSystemShape;
-let generatorPosition = [[0, 0, 0,0]] as KeyframesVec3;
-let generatorRotation = [[0, 0, 0,0]] as KeyframesVec3;
+let generatorShape = "Point" as ParticleSystemShape;
+let generatorType = "Wall" as ParticleSystemType;
+let generatorPosition = [[0, 0, 0, 0]] as KeyframesVec3;
+let generatorRotation = [[0, 0, 0, 0]] as KeyframesVec3;
 let generatorScale = [1, 1, 1] as Vec3;
 let generatorExpansion = 0;
+let generatorName = "gen";
 
 // Custom Stuff
-let particleAnimateDissolve = [[1,0]] as KeyframesLinear;
-let particleAnimateColor = [[1,1,1,1,0]] as KeyframesVec4;
-let particleAnimateScale = [[1,1,1,0]] as KeyframesVec3;
-let particleAnimateRotation = [[0,0,0,0]] as KeyframesVec3;
-let particleAnimateLocalRotation = [[0,0,0,0]] as KeyframesVec3;
+let particleAnimateDissolve = [[1, 0]] as KeyframesLinear;
+let particleAnimateColor = [[1, 1, 1, 1, 0]] as KeyframesVec4;
+let particleAnimateScale = [[1, 1, 1, 0]] as KeyframesVec3;
+let particleAnimateRotation = [[0, 0, 0, 0]] as KeyframesVec3;
+let particleAnimateLocalRotation = [[0, 0, 0, 0]] as KeyframesVec3;
 
 export class ParticleSystem {
 
-    constructor(timeStart: number, timeEnd: number, amount: number) {
-        generatorTime = timeStart;
-        generatorDuration = timeEnd - timeStart;
+    constructor(time: number, duration: number, amount: number) {
+        generatorTime = time;
+        generatorDuration = duration;
         generatorParticleAmount = amount;
     }
+
+
+    generator = new PSGenerator();
+
+
+    particle = new PSParticles();
+
 
     /**
      * Make the Particle System
      * @returns 
      */
     push() {
+
+        const particleTracks = [];
+
         for (let i = 1; i <= generatorParticleAmount; i++) {
 
             //// THE WALL
 
             const particleTime = rand(generatorTime, generatorTime + generatorDuration); // Calculate when the particle will spawn
-            const particleRandomX = rand(-generatorScale[0],generatorScale[0]);
-            const particleRandomY = rand(-generatorScale[1],generatorScale[1]);
-            const particleRandomZ = rand(-generatorScale[2],generatorScale[2]);
-            const particleRandomAX = rand(-generatorExpansion,generatorExpansion);
-            const particleRandomAZ = rand(-generatorExpansion,generatorExpansion);
+            const particleRandomX = rand(-generatorScale[0], generatorScale[0]);
+            const particleRandomY = rand(-generatorScale[1], generatorScale[1]);
+            const particleRandomZ = rand(-generatorScale[2], generatorScale[2]);
+            const particleRandomAX = rand(-generatorExpansion, generatorExpansion);
+            const particleRandomAZ = rand(-generatorExpansion, generatorExpansion);
+            const particleRandomDistanceX = rand(-particleDistance, particleDistance)
+            const particleRandomDistanceY = rand(-particleDistance, particleDistance)
+            const particleRandomDistanceZ = rand(-particleDistance, particleDistance)
+            let particle;
 
-            const particle = new Wall(generatorTime + rand(-particleStartThreshold, particleStartThreshold) + particleTime, particleLife + rand(-particleEndThreshold, particleEndThreshold)); // Make the particle
+            if (generatorType == "Wall") {
+                particle = new Wall();
+            } else {
+                particle = new Wall();
+            }
+
             particle.animate.rotation = generatorRotation; // Set the rotation of the particle generator
             particle.animate.position = generatorPosition; // Set the position of the particle generator
-            
+
 
             // PARTICLE SYSTEM SHAPE
-            if (generatorType == "Direction") {
-                particle.animate.definitePosition = [[particleRandomX,particleRandomY,particleRandomZ,0],[particleRandomX+particleRandomAX,particleRandomY+particleDistance,particleRandomZ+particleRandomAZ,1,particleEasing]];
-            } else if (generatorType == "Point") {
-                particle.animate.definitePosition = [[0,0,0,0], [...arrAdd(rotatePoint([0, particleDistance, 0], [rand(0, 360),rand(0, 360),rand(0, 360)]), 0), 1, particleEasing]];
+            if (generatorShape == "Direction") {
+                particle.animate.definitePosition = [[particleRandomX, particleRandomY, particleRandomZ, 0], [particleRandomX + particleRandomAX, particleRandomY + particleDistance, particleRandomZ + particleRandomAZ, 1, particleEasing]];
+            } else if (generatorShape == "Point") {
+                particle.animate.definitePosition = [[0, 0, 0, 0], [...arrAdd(rotatePoint([0, particleDistance, 0], [rand(0, 360), rand(0, 360), rand(0, 360)]), 0), 1, particleEasing]];
+            } else if (generatorShape == "Field") {
+                particle.animate.definitePosition = [[particleRandomX, particleRandomY, particleRandomZ, 0], [particleRandomX + particleRandomDistanceX, particleRandomY + particleRandomDistanceY, particleRandomZ + particleRandomDistanceZ, 1, particleEasing]]
             }
 
             particle.scale = [1, 1, 1];
@@ -68,66 +92,84 @@ export class ParticleSystem {
             particle.animate.rotation = [...particleAnimateRotation] as KeyframesVec3;
             particle.animate.localRotation = [...particleAnimateLocalRotation] as KeyframesVec3;
 
+            particle.life = particleLife;
+            particle.lifeStart = generatorTime + particleTime;
             particle.NJS = 0;
             particle.interactable = false;
-            particle.track.value = `particle${i}`
+            particle.track.value = generatorName + `Particle${i}`
+            particleTracks.push(particle.track.value);
             particle.push(true);
-
         }
+        new CustomEvent().assignTrackParent(particleTracks, generatorName + "PS").push();
     }
+}
 
+class PSGenerator {
 
-    get generatorTime() { return generatorTime; }
-    get generatorDuration() { return generatorDuration; }
-    get generatorParticleAmount() { return generatorParticleAmount; }
+    get time() { return generatorTime; }
+    get duration() { return generatorDuration; }
+    get particleAmount() { return generatorParticleAmount; }
 
-    get particleStartThreshold() { return particleStartThreshold; }
-    get particleEndThreshold() { return particleEndThreshold; }
-    get particleLife() { return particleLife; }
-    get particleDistance() { return particleDistance; }
-    get particleEasing() { return particleEasing; }
-
-    get generatorType() { return generatorType; }
-    get generatorPosition() { return generatorPosition; }
-    get generatorRotation() { return generatorRotation; }
-    get generatorScale() { return generatorScale; }
-    get generatorExpansion() { return generatorExpansion; }
-
-    get particleAnimateDissolve() { return particleAnimateDissolve; }
-    get particleAnimateColor() { return particleAnimateColor; }
-    get particleAnimateScale() { return particleAnimateScale; }
-    get particleAnimateRotation() { return particleAnimateRotation; }
-    get particleAnimateLocalRotation() { return particleAnimateLocalRotation; }
+    get shape() { return generatorShape; }
+    get type() { return generatorType; }
+    get position() { return generatorPosition; }
+    get rotation() { return generatorRotation; }
+    get scale() { return generatorScale; }
+    get expansion() { return generatorExpansion; }
+    get name() { return generatorName; }
 
 
 
-    set generatorTime(value: number) { generatorTime = value; }
-    set generatorDuration(value: number) { generatorDuration = value; }
-    set generatorParticleAmount(value: number) { generatorParticleAmount = value; }
+    set time(value: number) { generatorTime = value; }
+    set duration(value: number) { generatorDuration = value; }
+    set particleAmount(value: number) { generatorParticleAmount = value; }
 
-    set particleStartThreshold(value: number) { particleStartThreshold = value; }
-    set particleEndThreshold(value: number) { particleEndThreshold = value; }
-    set particleLife(value: number) { particleLife = value; }
-    set particleDistance(value: number) { particleDistance = value; }
-    set particleEasing(value: Interpolation) { particleEasing = value; }
-
-    set generatorType(value: ParticleSystemShape) { generatorType = value; }
-    set generatorPosition(value: KeyframesVec3) { generatorPosition = value; }
-    set generatorRotation(value: KeyframesVec3) { generatorRotation = value; }
-    set generatorScale(value: Vec3) { generatorScale = value; }
-    set generatorExpansion(value: number) { generatorExpansion = value; }
-
-    set particleAnimateDissolve(value: KeyframesLinear) { particleAnimateDissolve = value; }
-    set particleAnimateColor(value: KeyframesVec4) { particleAnimateColor = value; }
-    set particleAnimateScale(value: KeyframesVec3) { particleAnimateScale = value; }
-    set particleAnimateRotation(value: KeyframesVec3) { particleAnimateRotation = value; }
-    set particleAnimateLocalRotation(value: KeyframesVec3) { particleAnimateLocalRotation = value; }
+    set shape(value: ParticleSystemShape) { generatorShape = value; }
+    set type(value: ParticleSystemType) { generatorType = value; }
+    set position(value: KeyframesVec3) { generatorPosition = value; }
+    set rotation(value: KeyframesVec3) { generatorRotation = value; }
+    set scale(value: Vec3) { generatorScale = value; }
+    set expansion(value: number) { generatorExpansion = value; }
+    set name(value: string) { generatorName = value; }
 
 }
 
-type ParticleSystemShape = 
+class PSParticles {
+
+    get life() { return particleLife; }
+    get distance() { return particleDistance; }
+    get easing() { return particleEasing; }
+
+
+    set life(value: number) { particleLife = value; }
+    set distance(value: number) { particleDistance = value; }
+    set easing(value: Interpolation) { particleEasing = value; }
+
+    /** The animation of this Particel System. */
+    animate = new PSWallAnimation();
+
+}
+
+class PSWallAnimation {
+
+    get dissolve() { return particleAnimateDissolve; }
+    get color() { return particleAnimateColor; }
+    get scale() { return particleAnimateScale; }
+    get rotation() { return particleAnimateRotation; }
+    get localRotation() { return particleAnimateLocalRotation; }
+
+    set dissolve(value: KeyframesLinear) { particleAnimateDissolve = value; }
+    set color(value: KeyframesVec4) { particleAnimateColor = value; }
+    set scale(value: KeyframesVec3) { particleAnimateScale = value; }
+    set rotation(value: KeyframesVec3) { particleAnimateRotation = value; }
+    set localRotation(value: KeyframesVec3) { particleAnimateLocalRotation = value; }
+
+}
+
+type ParticleSystemShape =
     "Point" |
-    "Hemisphere" |
     "Direction" |
-    "Cone" |
-    "Box"
+    "Field"
+
+type ParticleSystemType =
+    "Wall"
